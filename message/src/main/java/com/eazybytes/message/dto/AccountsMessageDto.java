14cd678d29016007ef530b704b90abe12b339555
tienv1per <tienv1perds@gmail.com>
2024-11-05 00:00:24 +0700

    send event with kafka

---
+++ message/src/main/java/com/eazybytes/message/dto/AccountsMessageDto.java
@@ -0,0 +1,9 @@
+package com.eazybytes.message.dto;
+
+public record AccountsMessageDto(
+        Long accountNumber,
+        String name,
+        String email,
+        String mobileNumber
+) {
+}
