59ece9fbdbeeec7f7ceb7179261d9a76781525b2
tienv1per <tienv1perds@gmail.com>
2024-11-05 00:00:58 +0700

    SF message service

---
+++ accounts/src/main/java/com/eazybytes/accounts/dto/AccountsMessageDto.java
@@ -0,0 +1,9 @@
+package com.eazybytes.accounts.dto;
+
+public record AccountsMessageDto(
+        Long accountNumber,
+        String name,
+        String email,
+        String mobileNumber
+) {
+}
